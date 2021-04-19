36d169b3fba0f122ff34b1f8290f3e6db33d9c2f
Michael Dwyer <Mike.a.dwyer32@gmail.com>
2021-04-19 19:46:12 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const body = document.body;
+const element = document.createElement('div');
+//create an h1 element, set that to value of new header variable, then give the element an id and class name of victory, and change the innerHTML to flatiron
+
+body.remove('main');
+const newHeader = document.createElement('h1');
+//body.appendChild('h1');
+// body.appendChild(newHeader);
+//const newHeader = document.getElementsById('newHeader');
+newHeader.innerHTML = "<h1>victory</h1>";
+newHeader.id = "victory";
+newHeader.innerHTML = "is the champion"
